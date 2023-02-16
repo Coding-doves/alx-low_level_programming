74e83b8a5c96a56c2e76e680e098ec7b84bad3df
Ada <obenedicta4@gmail.com>
2023-02-16 12:25:13 -0800

    0x00 Hello_world changes made

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
+
+/**
+ *main in an entry point
+ *put ur code inside the main
+ *return 0 sends back a 'true' value
+ */
 
 int main(void){
 	puts("Programming is like building a multilingual puzzle");
