81a3f2ac78cd86e9829ce01f7a4b7b826c065c13
Ada <obenedicta4@gmail.com>
2023-02-16 12:58:17 -0800

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,10 @@
-#!/bin/bash
-
 #include <stdio.h>
 #include <stdlib.h>
+
+/**
+*main - Entry point
+*Return: Always 1
+*/
 
 int main(void){
 	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
