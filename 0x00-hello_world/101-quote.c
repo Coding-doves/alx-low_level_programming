3fa9508699db93288e34bc4ed0432348643cdb49
Ada <obenedicta4@gmail.com>
2023-02-17 08:15:12 -0800

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 /**
-*main - Entry point
-*Return: Always 1
-*/
-
-int main(void){
-	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-	return(1);	
+ * main - Entry point
+ * Return: Always 1
+ */ 
+int main(void)
+{
+	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);	
 }
