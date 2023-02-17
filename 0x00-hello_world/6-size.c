74dba893a04d12025e76ef42bd4fd6ef844838a7
Ada <obenedicta4@gmail.com>
2023-02-17 11:07:01 -0800

    holao mmm6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@ int main(void)
 	int b;
 	long int c;
 	long long int d;
-	float e;
+	float f;
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(b));
