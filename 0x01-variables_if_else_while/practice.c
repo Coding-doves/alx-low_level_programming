5d412400ebaa4576a4bd74ed38553b7f3917e757
Ada <obenedicta4@gmail.com>
2023-02-19 02:47:06 -0800

    0x01. C

---
+++ 0x01-variables_if_else_while/practice.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - is the entry point of my code
+ *
+ * Return: 0 for success
+ */
+
+int main(void)
+{
+	int num;
+
+	/* your code goes there */
+	return (0);
+}
