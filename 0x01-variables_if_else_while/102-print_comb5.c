a04d5e43b03d3c05abf9217db9dca51bb50f3832
Ada <obenedicta4@gmail.com>
2023-02-20 08:09:23 -0800

    102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,22 +12,24 @@ int main(void)
 
 	for (zero = 0; zero <= 98; zero++)
 	{
-	for (one = zero + 1; one <= 99; one++)
-	{
-		putchar((zero / 10) + '0');
-		putchar((zero % 10) + '0');
-		putchar(' ');
-		putchar((one / 10) + '0');
-		putchar((one % 10) + '0');
-
-		if ((zero == 98) && (one == 99))
+		for (one = zero + 1; one <= 99; one++)
 		{
-			continue;
+			putchar((zero / 10) + '0');
+			putchar((zero % 10) + '0');
+			putchar(' ');
+			putchar((one / 10) + '0');
+			putchar((one % 10) + '0');
+
+			if ((zero == 98) && (one == 99))
+			{
+				continue;
+			}
+			putchar(',');
+			putchar(' ');
 		}
-		putchar(',');
-		putchar(' ');
 	}
 
-	putchar('\n');
-	return (0);
+		putchar('\n');
+
+		return (0);
 }
