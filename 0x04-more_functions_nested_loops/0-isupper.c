dd93668469c8613808176b53ddf067be533f5e6d
Ada <obenedicta4@gmail.com>
2023-02-23 07:14:46 -0800

    upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * main - check the code.
- *
+ * _isupper - check the code.
+ *@c: variable
  * Return: Always 0.
  */
 int _isupper(int c)
 {
 	if (c >=  'A' && c <= 'Z')
-		return(1);
+		return (1);
 	else
 		return (0);
 }
