743530943e41db92ce61a8779b15740c439fc458
Ada <obenedicta4@gmail.com>
2023-02-23 11:37:40 -0800

    4-print

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,22 +2,21 @@
 
 /**
  * more_numbers - prints 0 -14, ten times
- * @i: container carrying ten times
- * @j: container carrying 0-14
- * Return: 0
+ * Return: void
  */
 
 void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 1; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
+			if (j >= 10)
 			_putchar(i);
-			_putchar(j);
+			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
-	}	
+	}
 }
