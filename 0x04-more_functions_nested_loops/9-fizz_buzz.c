305c8353532418bc62a6c5159ef9b97d4306dbee
Ada <obenedicta4@gmail.com>
2023-02-24 11:29:01 -0800

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -15,13 +16,15 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-			printf(" %s", fb);
+			printf("%s", fb);
 		else if (i % 3 == 0)
-			printf(" %s", f);
+			printf("%s", f);
 		else if (i % 5 == 0)
-			printf(" %s", b);
+			printf("%s", b);
 		else
-			printf(" %d", i);
+			printf("%d", i);
+		if (i != 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
