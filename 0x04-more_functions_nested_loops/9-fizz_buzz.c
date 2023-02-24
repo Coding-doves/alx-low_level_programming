d1af7d087e65400f0f102b043bb98d1f06744dff
Ada <obenedicta4@gmail.com>
2023-02-24 11:43:54 -0800

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,29 +2,39 @@
 #include <stdio.h>
 
 /**
- * main - using the mulitpies of 3 and 5 print fizzbuzz
+ * main -  print fizzbuzz using the mulitpies of 3 and 5
  * Return: 0
  */
 
 int main(void)
 {
 	int i;
-	char f[] = "fizz";
-	char b[] = "buzz";
-	char fb[] = "fizzbuzz";
+	char f[] = "Fizz";
+	char b[] = "Buzz";
+	char fb[] = "FizzBuzz";
 
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
+		{
 			printf("%s", fb);
+		}
 		else if (i % 3 == 0)
+		{
 			printf("%s", f);
+		}
 		else if (i % 5 == 0)
+		{
 			printf("%s", b);
+		}
 		else
+		{
 			printf("%d", i);
+		}
 		if (i != 100)
+		{
 			printf(" ");
+		}
 	}
 	printf("\n");
 	return (0);
