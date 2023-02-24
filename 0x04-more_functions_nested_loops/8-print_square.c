67df4825b3b71ae45ce132714ebc5fba50748c85
Ada <obenedicta4@gmail.com>
2023-02-24 10:58:57 -0800

    square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,15 +10,20 @@ void print_square(int size)
 {
 	int j, i;
 
-	for (j = 0; j <= size; j++)
+	if (size <= 0)
 	{
-		for (i = 0; i <= size; i++)
+		_putchar('\n');
+	}
+	else
+	{
+		for (j = 0; j < size; j++)
 		{
-			_putchar('#');
+			for (i = 0; i < size; i++)
+			{
+				_putchar('#');
+			}
 
-			if (i <= 0)
-				_putchar('\n');
+			_putchar('\n');
 		}
 	}
-	_putchar('\n');
 }
