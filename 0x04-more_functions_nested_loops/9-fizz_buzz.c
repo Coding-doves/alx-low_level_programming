a8471f8bc64e598814341e3c74f7a1acdf25a6d5
Ada <obenedicta4@gmail.com>
2023-02-23 16:14:00 -0800

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
+
 /**
- * FizzBuzz - using the mulitpies of 3 and 5 print fizzbuzz
- * @f: is the container for fizz
- * @b: is container for buzz
- * @fb: is the container for fizz buzz
+ * main - using the mulitpies of 3 and 5 print fizzbuzz
  * Return: 0
  */
 
 int main(void)
-{       
+{
 	int i;
 	char f[] = "fizz";
 	char b[] = "buzz";
-        char fb[] = "fizzbuzz";
+	char fb[] = "fizzbuzz";
 
-	for (i = 0; i <= 100; i++)
-        {
-                if (i % 3 == 0 && i % 5 == 0)
-                        printf("%s", fb);
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("%s", fb);
 		else if (i % 3 == 0)
 			printf("%s", f);
 		else if (i % 5 == 0)
@@ -25,7 +23,7 @@ int main(void)
 		else
 			printf("%d", i);
 		printf(" ");
-        }
+	}
 	printf("\n");
 	return (0);
-}	
+}
