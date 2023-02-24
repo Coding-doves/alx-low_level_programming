51768fb6e89f2226ca7adff117bb41ecf0230f6e
Ada <obenedicta4@gmail.com>
2023-02-24 10:29:36 -0800

    diagonals

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,18 +10,20 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	for (i = 1; i <= n; i++)
+	if (n <= 0)
 	{
-		_putchar('\\');
 		_putchar('\n');
-
-		for (j = 1; j <= i; j++)
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
 		{
-			_putchar(' ');
-
-			if (j == 0)
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+				_putchar('\\');
 				_putchar('\n');
 		}
 	}
-	_putchar('\n');
 }
