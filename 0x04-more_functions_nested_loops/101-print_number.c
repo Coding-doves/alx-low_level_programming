bb87649b6d32da8c3860b893ba0f2387f373ea8d
Ada <obenedicta4@gmail.com>
2023-02-24 22:26:49 -0800

    task complete

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,12 +2,23 @@
 
 /**
  * print_number - print integer values
- *
+ * @n: num (integer) to print
  * Returns: void
  */
 
 void print_number(int n)
 {
-	_putchar(n);
-	_putchar('\n');
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		i = i * -1;
+		i = n;
+		_putchar('-');
+	}
+
+	i = i / 10;
+	if (i != 0)
+		print_number(i);
+	_putchar((unsigned int) n % 10 + '0');
 }
