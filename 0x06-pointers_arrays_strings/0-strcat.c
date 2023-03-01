1595dbc2f216fd56ebc4bbefb11efb2b3477ff3e
Ada <obenedicta4@gmail.com>
2023-03-01 03:15:22 -0800

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcat - fuction to concat two words
+ * @dest: string 1
+ * @src: string 2
+ * Return: dest 
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int  i;
+	int a;
+
+	for (i = 0; i < dest[i]; i++)
+	{
+		dest[i];
+	}
+
+	for (a = 0; a < src[a]; a++)
+	{
+		dest[i] += src[j];
+	}
+
+	return (dest);
+}
