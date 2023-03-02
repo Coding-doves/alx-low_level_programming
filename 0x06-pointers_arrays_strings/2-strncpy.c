50960f67b17dbd2d3b312548b5e638949ecbb93c
Ada <obenedicta4@gmail.com>
2023-03-02 00:42:12 -0800

    more pointers

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,23 +10,23 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int j;
 	int i;
-
-	j = 0;
-	while (dest[j] != '\0')
-	{
-		j++;
-	}
+	int j;
 
 	i = 0;
-	while (i < n && src[i] != '\0')
+	while (dest[i] != '\0')
 	{
-		dest[j] = src[i];
-		j++;
 		i++;
 	}
 
-	dest[j] = '\0';
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
 	return (dest);
 }
