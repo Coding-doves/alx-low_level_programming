600d85ab8c3cb06a86bcd7fe81b5aa8b1a2f95fa
Ada <obenedicta4@gmail.com>
2023-03-06 13:49:37 -0800

    0x07

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - this function fills memory with a constant byte.
+ * @s: pointer
+ * @b: source
+ * @n: number of bytes of the memory area to be filled
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+
+	return (s);
+}
