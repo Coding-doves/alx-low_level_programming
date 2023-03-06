857bce5004481e9f2c001a6a67dbaf1dfb89b5c1
Ada <obenedicta4@gmail.com>
2023-03-06 13:35:49 -0800

    0x07

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memcpy - a function for copying memory area
+ * @dest: destination of the copied bytes
+ * @src: source to copy from
+ * @n: number of bytes to be copied
+ * Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	if (dest == NULL)
+		return (NULL);
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
