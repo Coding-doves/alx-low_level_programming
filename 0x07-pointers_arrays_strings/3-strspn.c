75e40a7d1416006a0f58fdfcd16adba0810b7a6b
Ada <obenedicta4@gmail.com>
2023-03-06 15:30:20 -0800

    0x07

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -16,7 +16,7 @@ unsigned int _strspn(char *s, char *accept)
 	{
 		bool seen = false;
 
-		for (j = 0; accept[j] != '\0')
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (accept[j] == s[i])
 			{
