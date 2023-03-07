bb03576f682387609bd685b33681264fcbe58f71
Ada <obenedicta4@gmail.com>
2023-03-07 00:46:42 -0800

    0x07

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,20 +11,20 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, j, row, col, sum = 0;
+	int i, j, sum = 0;
 
-	for (i = 0; i < row; i++)
+	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < col; j++)
+		for (j = 0; j < size; j++)
 		{
 			printf("%d\n", a[i][j]);
 		}
 		printf("\n");
 	}
 
-	for (i = 0; i < row; i++)
+	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < col; j++)
+		for (j = 0; j < size; j++)
 		{
 			if (i == j)
 			{
