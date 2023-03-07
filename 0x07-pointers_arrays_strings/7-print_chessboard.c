b5fb3fabaf5bca632f8c6920f7bfed85c5547cac
Ada <obenedicta4@gmail.com>
2023-03-07 04:55:03 -0800

    0x07

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,22 +8,22 @@
 
 void print_chessboard(char (*a)[8]);
 {
-  int n, j;
+	int n, j;
 
-        n = 0;
+	n = 0;
 
-        while (n < 8) 
-        {
-                j = 0;
+	while (n < 8)
+	{
+		j = 0;
 
-                while (j < 8)
-                {
-                        _putchar (a[n][j]);
-                        j++;
-                }
+		while (j < 8)
+		{
+			_putchar (a[n][j]);
+			j++;
+		}
 
-                _putchar ('\n'); /print new line/
-                n++;
-        }
+		_putchar ('\n');
+		n++;
+	}
 }
 
