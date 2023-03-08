33db24c1e3f63367d84175eeb4c50edaa69def97
Ada <obenedicta4@gmail.com>
2023-03-08 12:59:37 -0800

    0x08

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,13 +1,24 @@
 #include "main.h"
 
 /**
- * _pow_recursion - 
- * @x:
- * @y:
- * Return:
+ * _pow_recursion - raise to the power
+ * @x: given number
+ * @y: raise to the power number
+ * Return: value of the exponentiation
  */
 
 int _pow_recursion(int x, int y)
 {
-
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
 }
