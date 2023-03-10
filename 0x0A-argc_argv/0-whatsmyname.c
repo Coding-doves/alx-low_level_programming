dae7a09d87e66a3c48510a99f4cb4c4227055f54
Ada <obenedicta4@gmail.com>
2023-03-10 09:19:52 -0800

    0x0A

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: rep num of things entered into the command line(on the terminal)
+ * @argv: an array that holds the string value of the things entered on the
+ * command line(in the terminal)
+ * Return: 0
+ */
+
+int main(int argc, char* argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
