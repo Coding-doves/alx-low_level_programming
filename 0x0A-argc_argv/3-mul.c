b0ee9572fda5162fbf1729fe3fab9a3060d9034e
Ada <obenedicta4@gmail.com>
2023-03-10 10:48:35 -0800

    0x0A

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * @argc: rep num of things entered into the command line(on the terminal)
+ * @argv: an array that holds the string value of the things entered on the
+ * command line(in the terminal)
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 1;
+	int mul;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	if (argc > 1 || argc < 3)
+	{
+		for (; i < argc; i++)
+			mul = atoi(argv[1]) * atoi(argv[2]);
+
+		printf("%d\n", mul);
+	}
+	return (0);
+}
