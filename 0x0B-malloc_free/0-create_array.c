805fc718dc7e590b51151333108d0d318676fff1
Ada <obenedicta4@gmail.com>
2023-03-13 18:00:08 -0700

    0x0A

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,10 +16,10 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	if (ch == NULL)
-		return (NULL);
-
 	ch = malloc(sizeof(char) * size);
+
+	if (ch == NULL)
+                return (NULL);
 
 	for (i = 0; i < size; i++)
 		ch + i = c;
