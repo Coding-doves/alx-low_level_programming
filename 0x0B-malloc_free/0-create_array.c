7cd9d1e4065248ec7cedbcd5d3ba2a5d874f34ee
Ada <obenedicta4@gmail.com>
2023-03-13 18:08:40 -0700

    0x0A

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	ch = malloc(sizeof(char) * size);
+	ch = (char *)malloc(sizeof(char) * size);
 
 	if (ch == NULL)
                 return (NULL);
