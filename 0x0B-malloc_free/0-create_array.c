d96944d0afe266a318f9628bbec0cefabf985eaf
Ada <obenedicta4@gmail.com>
2023-03-13 17:52:05 -0700

    0x0A

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * create_array - a function that creates an array of chars,
+ * and initializes it with a specific char.
+ * @size: size of array
+ * @c: specific char for initialization
+ * Return: pointer to char
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ch;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	if (ch == NULL)
+		return (NULL);
+
+	ch = malloc(sizeof(char) * size);
+
+	for (i = 0; i < size; i++)
+		ch + i = c;
+
+	return (ch);
+}
