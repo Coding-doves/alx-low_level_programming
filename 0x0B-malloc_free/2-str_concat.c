a2ab6b1629e51853bf3d61f9e8fd21ee912a346e
Ada <obenedicta4@gmail.com>
2023-03-13 23:12:35 -0700

    0x0B

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -28,9 +28,9 @@ char *str_concat(char *s1, char *s2)
 	char *cat;
 
 	if (s1 == NULL)
-		s1 = " ";
+		s1 = "";
 	if (s2 == NULL)
-		s2 = " ";
+		s2 = "";
 
 	cat = (char *)malloc(sizeof(char) * (_strlen(s1) + _strlen(s2) + 1));
 
