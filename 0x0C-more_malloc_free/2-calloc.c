f8e03a7c9f2c2a62ecbfb44d87d52963fbf76d1f
Ada <obenedicta4@gmail.com>
2023-03-16 15:40:00 -0700

    0x0C

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -20,7 +20,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (ptr == NULL)
 		return (NULL);
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < ptr; i++)
 		ptr[i] = 0;
 
 	return (ptr);
