7a828b70fa0f8e1c62a8c0fe1c92a476b64226ee
Ada <obenedicta4@gmail.com>
2023-03-16 15:31:35 -0700

    0x0C

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - function that allocates memory for an array, using malloc
+ * @nmemb: array
+ * @size: bytes
+ * Return: pointer to the allocated memory.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *ptr, i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(sizeof(nmemb) * size);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		ptr[i] = 0;
+
+	free(ptr);
+	return (ptr);
+}
