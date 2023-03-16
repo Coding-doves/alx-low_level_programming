a9d2fdb8ec078ec124f5b1eeb4f884d8c4fc14d6
Ada <obenedicta4@gmail.com>
2023-03-16 16:19:55 -0700

    0x0C

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,14 +4,14 @@
 /**
  * malloc_checked - function that allocates memory using malloc
  * @b: variable
- * Returns: a pointer to the allocated memory
+ * Return: a pointer to the allocated memory
  */
 
 void *malloc_checked(unsigned int b)
 {
 	unsigned int *ptr;
 
-	ptr = malloc(sizeof(b));
+	ptr = malloc(b);
 
 	if (ptr == NULL)
 		exit(98);
