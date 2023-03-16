041ca30074e94494c09260698c840d67d0586afe
Ada <obenedicta4@gmail.com>
2023-03-16 16:16:31 -0700

    0x0C

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - function that allocates memory using malloc
+ * @b: variable
+ * Returns: a pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *ptr;
+
+	ptr = malloc(sizeof(b));
+
+	if (ptr == NULL)
+		exit (98);
+
+	return (ptr);
+}
