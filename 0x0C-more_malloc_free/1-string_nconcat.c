ace984bb0fe83bbcfb3c76f0f9751cd2bb2ece6d
Ada <obenedicta4@gmail.com>
2023-03-16 21:48:03 -0700

    0x0C

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -30,7 +30,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	ptr = malloc(sizeof(char) * _strlen(s1) + _strlen(s2));
+	ptr = malloc(sizeof(char) * _strlen(s1) + _strlen(s2) - 1);
 
 	if (ptr == NULL)
 		return (NULL);
