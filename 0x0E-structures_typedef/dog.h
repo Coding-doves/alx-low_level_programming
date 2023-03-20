ad784114339d1c0c280152317b97a38912a21d0b
Ada <obenedicta4@gmail.com>
2023-03-20 06:33:55 -0700

    0x0E

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOGTYPE
+#define DOGTYPE
+
+/**
+ * struct dog - Define a new type
+ * @name: of dog
+ * @age: of dog
+ * @owner: of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
