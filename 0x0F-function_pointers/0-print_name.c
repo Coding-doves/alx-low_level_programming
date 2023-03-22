eb420dbaf0d7e527a87e51d074b21cc9e4a8f72b
Ada <obenedicta4@gmail.com>
2023-03-22 04:32:15 -0700

    0x0F

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - a function that prints a name.
+ * @name: pointer
+ * @f: function pointer
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	int i;
+
+	if (name)
+	{
+		for (i = 0; name[i]; i++)
+		{
+			f(&name[i]);
+		}
+	}
+}
