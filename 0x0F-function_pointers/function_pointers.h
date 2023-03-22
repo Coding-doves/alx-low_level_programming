8b61288809ab76323392478447f6ea0db072e05d
Ada <obenedicta4@gmail.com>
2023-03-22 05:07:44 -0700

    0x0F

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,7 @@
 #ifndef _FUNC_POINTER_
 #define _FUNC_POINTER_
 
-int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
