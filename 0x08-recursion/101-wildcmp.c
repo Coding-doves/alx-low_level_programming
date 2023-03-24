1d5e2c520bd9f698701de6d3c00bb743aaf091af
Ada <obenedicta4@gmail.com>
2023-03-24 01:52:05 -0700

    wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -18,10 +18,10 @@ int wildcmp(char *s1, char *s2)
 
 		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	}
-	
-	if (*s1 != *s2)
-		return 0;
 
-	return wildcmp(s1 + 1, s2 + 1); // recursively compare the rest of the strings
+	if (*s1 != *s2)
+		return (0);
+
+	return (wildcmp(s1 + 1, s2 + 1));
 }
 
