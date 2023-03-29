4d5f14bf872694d4c6db0ccd42a81a0cecbb5609
Ada <obenedicta4@gmail.com>
2023-03-29 03:58:09 -0700

    malloc

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,95 +1,68 @@
 #include <stdlib.h>
 #include <string.h>
 
-int count_words(char *str);
-int next_word_length(char *str);
-char *copy_next_word(char *str, int length);
+#define DELIMITER " "
+
+int count_words(char *str)
+{
+	int count = 0;
+	int is_word = 0;
+
+	while (*str)
+	{
+		if (*str == ' ')
+			is_word = 0;
+		else if (!is_word)
+		{
+			count++;
+			is_word = 1;
+		}
+		str++;
+	}
+
+	return (count);
+}
 
 char **strtow(char *str)
 {
-	int num_words, word_index, word_length;
+	int i, j;
 	char **words;
+	int word_count;
 
-	if (str == NULL || str[0] == '\0')
-		return NULL;
+	if (str == NULL || *str == '\0')
+		return (NULL);
 
-	num_words = count_words(str);
+	word_count = count_words(str);
+	words = (char **)malloc(sizeof(char *) * (word_count + 1));
 
-	words = (char**)malloc((num_words + 1) * sizeof(char*));
 	if (words == NULL)
-		return NULL;
+		return (NULL);
 
-	word_index = 0;
-	while (*str != '\0'){
+	i = 0;
+	while (*str)
+	{
 		while (*str == ' ')
 			str++;
 
 		if (*str == '\0')
-			break;
+   	         break;
 
-	word_length = next_word_length(str);
-        char *word = copy_next_word(str, word_length);
-        if (word == NULL) {
-            for (int i = 0; i < word_index; i++) {
-                free(words[i]);
-            }
-            free(words);
-            return NULL;
-        }
-        words[word_index++] = word;
+		j = 0;
+		while (str[j] != ' ' && str[j] != '\0')
+		j++;
 
-	str += word_length;
+		words[i] = (char *)malloc(sizeof(char) * (j + 1));
+		if (words[i] == NULL)
+			return (NULL);
+
+		strncpy(words[i], str, j);
+		words[i][j] = '\0';
+		i++;
+
+		str += j;
 	}
 
-	words[num_words] = NULL;
+	words[word_count] = NULL;
 
-	return words;
+	return (words);
 }
-
-int count_words(char *str)
-{
-	int count = 0, in_word = 0;
-
-	while (*str != '\0')
-	{
-		if (*str == ' ' && in_word)
-			in_word = 0;
-
-		else if (*str != ' ' && !in_word)
-		{
-			in_word = 1;
-			count++;
-		}
-		str++;
-	}
-	return count;
-}
-
-int next_word_length(char *str)
-{
-	int length = 0;
-
-	while (*str != '\0' && *str != ' ')
-	{
-		length++;
-        	str++;
-	}
-	return length;
-}
-
-char *copy_next_word(char *str, int length)
-{
-	int i;
-	char *word;
-
-	word = (char*)malloc((length + 1) * sizeof(char));
-	if (word == NULL)
-		return NULL;
-
-	for (i = 0; i < length; i++)
-		word[i] = *str++;
-	
-	word[length] = '\0';
-	return word;
-}
-
