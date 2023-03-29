3edfb5224ccecd32746c40d70a98c02c19df5332
Ada <obenedicta4@gmail.com>
2023-03-28 19:19:37 -0700

    panlin

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -8,13 +8,16 @@
 
 int check_palin_len(char *s)
 {
-	if (s[0] == '\0')
-		return (0);
+	int len = strlen(s);
 
-	if (s[1] == '\0')
-		return (1);
-	else
-		return (check_palin_len(&s[1]);
+	for (int i = 0; i < len / 2; i++)
+	{
+		if (s[i] != s[len - i - 1])
+		{
+			return 0;
+		}
+	}
+	return 1;
 }
 
 /**
