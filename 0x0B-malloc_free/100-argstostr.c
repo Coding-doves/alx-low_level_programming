65a890b0b965b4d172af73be30c7717274cfd095
Ada <obenedicta4@gmail.com>
2023-03-29 04:08:39 -0700

    malloc

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -17,7 +17,7 @@ char *argstostr(int ac, char **av)
 		return (NULL);
 
 	for (i = 0; i < ac; i++)
-		length += strlen(av[i]) + 1;
+		length += strlen(av[i]);
 
 	str = (char *)malloc(length * sizeof(char));
 
