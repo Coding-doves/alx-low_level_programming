2d6f2acb2e931619713eb16b53cf61ba2fc8078f
Ada <obenedicta4@gmail.com>
2023-03-30 04:48:45 -0700

    singly linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,20 +3,20 @@
 /**
  * print_list -  function that prints all the elements of a list_t list
  * @h: head of linked list
- * Return: number of nodes or nil if empty
+ * Return: number of nodes
  */
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 	const list_t *temp = NULL;
 
-	if (h->str == NULL)
-		printf("[0] (nil)\n");
-
 	temp = h;
 	while (temp != NULL)
 	{
-		printf("[%d] %s\n", temp->len, temp->str);
+		if (temp->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", temp->len, temp->str);
 
 		count++;
 		temp = temp->next;
