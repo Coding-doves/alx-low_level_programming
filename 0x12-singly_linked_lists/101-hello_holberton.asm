c6cbaee0e672d6feb06a5ffe8976d5c801f2df3e
Ada <obenedicta4@gmail.com>
2023-03-30 13:16:03 -0700

    singly linked list

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,20 @@
+global main
+
+section .data
+        msg: db "Hello, Holberton", 0xA
+        msglength: equ $ - msg
+
+section .text
+main:
+        ; Write the message to stdout
+        mov eax, 0x4         ; System call number for write
+        mov ebx, 0x1         ; File descriptor for stdout
+        mov ecx, msg         ; Address of message to write
+        mov edx, msglength   ; Length of message
+        int 0x80             ; Call kernel
+
+        ; Exit the program with status code 0
+        mov eax, 0x1         ; System call number for exit
+        xor ebx, ebx         ; Status code 0
+        int 0x80             ; Call kernel
+
