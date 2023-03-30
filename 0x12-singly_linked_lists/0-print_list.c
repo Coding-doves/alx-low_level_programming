3d28b68f7039fe3f745b93bdfa74af8ec7dabfd5
Ada <obenedicta4@gmail.com>
2023-03-30 06:28:12 -0700

    singly linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,14 +7,14 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	unsigned int count = 0;
 	const list_t *temp = NULL;
 
 	temp = h;
 	while (temp != NULL)
 	{
 		if (temp->str == NULL)
-			printf("[%d] (nil)\n", 0);
+			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", temp->len, temp->str);
 
---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * add_node - function that adds a new node at the beginning of a list_t list
+ * @head: pointer to first list
+ * @str: new node to add at the begining of the list
+ * Return: the address of the new element, or NULL if it failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *temp = NULL;
+
+	if (head == NULL || str == NULL)
+		return (NULL);
+
+	temp = malloc(sizeof(list_t));
+		if (temp == NULL)
+			return (NULL);
+
+	temp->str = strdup(str);
+	if (temp->str == NULL)
+		return (NULL);
+
+	temp->len = strlen(str);
+	temp->next = *head;
+	*head = temp;
+
+	return (temp);
+}
