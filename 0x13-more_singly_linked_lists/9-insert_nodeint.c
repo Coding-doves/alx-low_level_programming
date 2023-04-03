4b0f4fe56b09404f0a11be24365c36489abc4753
Ada <obenedicta4@gmail.com>
2023-04-03 15:06:15 -0700

    More singly linked list

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts a new node at a given position
+ * @head: pointer to first node
+ * @idx: position
+ * @n: new node
+ * Return: the address of the new node, or NULL if it failed
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *ptr, *new;
+
+	if (head == NULL)
+		return (NULL);
+
+	ptr = *head;
+	new = malloc(sizeof(listint_t));
+	new->n = n;
+	new->next = NULL;
+
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+	}
+	while (idx != 1)
+	{
+		if (ptr != NULL)
+			ptr = ptr->next;
+		else
+			return (NULL);
+		idx--;
+	}
+	new->next = ptr->next;
+	ptr->next = new;
+	return (new);
+}
