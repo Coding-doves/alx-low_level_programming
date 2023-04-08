82112bc47186fd87d4c25f216792c2a7665b49ca
Ada <obenedicta4@gmail.com>
2023-04-08 15:53:31 -0700

    file i/o (handling)

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,55 @@
+#include "main.h"
+
+/**
+ * _strlen - get length of string
+ * @s: string
+ * Return: i
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	return (i);
+}
+
+/**
+ * create_file - creates a file.
+ * @filename: file
+ * @text_content: string
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int p;
+	ssize_t lent, pas;
+
+	p = open(filename, O_WRONLY | O_CREAT | O_EXCL, 0600);
+
+	if (p == -1)
+	{
+		p = open(filename, O_WRONLY | O_TRUNC);
+		if (p == -1)
+			return (-1);
+	}
+
+	if (filename == NULL)
+		return (-1);
+	
+	if (text_content != NULL)
+	{
+		lent = _strlen(text_content);
+		pas = write(p, text_content, lent);
+		if (pas == -1)
+		{
+			close(p);
+			return (-1);
+		}
+	}
+
+	close(p);
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,9 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 #include <stddef.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
