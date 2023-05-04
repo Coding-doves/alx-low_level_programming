5e544da96a2ca36d9149141d4238840a0c25edd8
Ada <obenedicta4@gmail.com>
2023-05-04 14:22:55 -0700

    bit manipulation

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * set_bit - sets the value of a bit to 1 at a given index
+ * @n: input
+ * @index:  bit you want to set
+ * Return: 1 or -1 if an error occured
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask = 1UL;
+
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
+	if (n == NULL)
+		return (-1);
+
+	mask <<= index;
+	return (*n |= mask);
+}
