7316b67364328c22ccae525a63c0b85ebf7b5b60
Ada <obenedicta4@gmail.com>
2023-05-04 13:15:02 -0700

    bit manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,9 +9,18 @@
 void print_binary(unsigned long int n)
 {
 	unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+	unsigned long int p = 0;
 
+	for(; mask != 0; mask >>= 1)
+	{
 		if (mask & n)
+		{
+			p = 1;
 			_putchar('1');
-		else
+		}
+		else if (p)
+			_putchar('0');
+	}
+		if (!p)
 			_putchar('0');
 }
