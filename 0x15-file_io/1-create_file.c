c24584e0b77e0dac68e9282e412c7cedc1e7f024
Ada <obenedicta4@gmail.com>
2023-05-08 15:15:07 -0700

    file input and output

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,10 +25,11 @@ int create_file(const char *filename, char *text_content)
 {
 	int fil;
 	ssize_t n = 0;
+	mode_t mode = S_IRUSR | S_IWUSR;
 
 	if (filename == NULL || text_content == NULL)
 		return (-1);
-	fil = open(filename, O_WRONLY | O_CREAT | O_TRUNC | O_EXCL, 600);
+	fil = open(filename, O_WRONLY | O_CREAT | O_TRUNC | O_EXCL, mode);
 
 	if (fil == -1)
 		return (-1);
