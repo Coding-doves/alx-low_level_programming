726fba31f121c116d5b9533691239397b70b0aaf
Ada <obenedicta4@gmail.com>
2023-05-09 02:08:31 -0700

    file input and output

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,18 +11,18 @@ ssize_t str_len(const char *s);
 /**
 * main - copy of a file to another
 * @argc: number of argument
-* @argv: string 
+* @argv: string
 * Return: 0 on success, exit_failure on failure
 */
 int main(int argc, char **argv)
 {
-    if (argc != 3)
-    {
-        dprintf(2, "Usage: cp %s file_to\n", argv[0]);
-        exit(97);
-    }
-    cp_to_file(argv[1], argv[2]);
-    return (0);
+	if (argc != 3)
+	{
+		dprintf(2, "Usage: cp %s file_to\n", argv[0]);
+		exit(97);
+	}
+	cp_to_file(argv[1], argv[2]);
+	return (0);
 }
 /**
 * cp_to_file -copy of a file to another
@@ -32,52 +32,65 @@ int main(int argc, char **argv)
 */
 int cp_to_file(const char *file_from, char *file_to)
 {
-int fd, fd2;
-ssize_t n, a, st[1024];
-mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	int fd, fd2;
+	ssize_t n, a;
+	char *st = malloc(1024);
+	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
-if (file_from == NULL || file_to == NULL)
-return (-1);
+	if (file_from == NULL || file_to == NULL)
+		return (-1);
 
-fd = open(file_from, O_RDONLY);
-if (fd == -1)
-{
-dprintf(2, "Error: Can't read from file %s", file_from);
-exit(98);
-}
-fd2 = open(file_to, O_WRONLY | O_CREAT | O_TRUNC | O_EXCL, mode);
-if (fd2 == -1)
-{
-dprintf(2, "Error: Can't write to %s", file_to);
-exit(99);
-}
+	fd = open(file_from, O_RDONLY);
+	if (fd == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+	fd2 = open(file_to, O_WRONLY | O_CREAT | O_TRUNC | O_EXCL, mode);
+	if (fd2 == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", file_to);
+		exit(99);
+	}
 
-while((n = read(fd, st, str_len(file_from))) > 0)
-{
-    a = write(fd2, st, n);
-    if (a != n)
-    {
-	dprintf(2, "Error: Can't write to %s", file_to);
-    	exit(99);
-    }
-}
-if (n == -1)
-{
-dprintf(2, "Error: Can't read from file %s", file_from);
-exit(98);
-}
+	if (st == NULL)
+	{
+		dprintf(2, "Error: Allocation failed\n");
+		exit(1);
+	}
+	while ((n = read(fd, st, str_len(file_from))) > 0)
+	{
+		if (n >= (1024 - 1))
+			st = realloc(st, 1024);
+		if (st == NULL)
+		{
+			dprintf(2, "Error: Allocation failed\n");
+			exit(1);
+		}
+		a = write(fd2, st, n);
+		if (a != n)
+		{
+			dprintf(2, "Error: Can't write to %s\n", file_to);
+			exit(99);
+		}
+	}
+	if (n == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
 
-if (close(fd)  == -1)
-{
-dprintf(2, "Error: Can't close fd %d\n", fd);
-exit(100);
-}
-if (close(fd2) == -1)
-{
-dprintf(2, "Error: Can't close fd %d\n", fd2);
-exit(100);
-}
-return (0);
+	if (close(fd)  == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+	if (close(fd2) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd2);
+		exit(100);
+	}
+	return (0);
 }
 
 /**
@@ -87,9 +100,9 @@ return (0);
 */
 ssize_t str_len(const char *s)
 {
-        ssize_t i;
+	ssize_t i;
 
-        for (i = 0; s[i]; i++)
-                ;
-        return (i);
+	for (i = 0; s[i]; i++)
+		;
+	return (i);
 }
