f4beecc21b45d94afe2fd2ad3c3534084410cc8a
Ada <obenedicta4@gmail.com>
2023-05-08 21:21:24 -0700

    file input and output

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -37,13 +37,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 	{
-		write(fil, " ", 1);
-	}
-	n = write(fil, text_content, str_len(text_content));
-	if (n != str_len(text_content))
-	{
-		close(fil);
-		return (-1);
+		n = write(fil, text_content, str_len(text_content));
+		if (n != str_len(text_content))
+		{
+			close(fil);
+			return (-1);
+		}
 	}
 
 	close(fil);
