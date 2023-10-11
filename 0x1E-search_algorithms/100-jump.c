f73dbf5e097a5e4e0dc4c495ee6d70757e36b8dc
Ada <106437177+Coding-doves@users.noreply.github.com>
2023-10-11 14:45:27 +0100

    Update 100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -18,33 +18,23 @@ return ((a < b) ? a : b);
  * Return: value index or -1 if not in array
 */
 int jump_search(int *array, size_t size, int value)
-{
-size_t prev_jump = 0;
-size_t jump = sqrt(size);
+{ size_t prev_jump = 0, jump = sqrt(size);
 
 while (array[min(jump, size) - 1] < value)
-{
-printf("Value checked array[%ld] = [%d]\n", prev_jump, array[prev_jump]);
+{ printf("Value checked array[%ld] = [%d]\n", prev_jump, array[prev_jump]);
 prev_jump = jump;
 jump += sqrt(size);
-
 if (prev_jump >= size)
 return (-1);
 }
-
 printf("Value found between indexes [%ld] and [%ld]\n", prev_jump, array[prev_jump]);
 while (array[prev_jump] < value)
-{
-printf("Value checked array[%ld] = [%d]\n", prev_jump, array[prev_jump]);
+{ printf("Value checked array[%ld] = [%d]\n", prev_jump, array[prev_jump]);
 prev_jump++;
 if (prev_jump == min(jump, size))
 return (-1);
 }
-
 if (array[prev_jump] == value)
-{
-printf("Value checked array[%ld] = [%d]\n", prev_jump, array[prev_jump]);
 return (prev_jump);
-}
 return (-1);
 }
